0c0011ce37b7d841a122cdade8184bf1d26d085e
JonyPlo <joonyyplo@gmail.com>
2022-09-04 22:21:46 -0300

    Se cambio la apikey de las imagenes y se creo la carpeta docs, solo eso

--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -1,14 +1,18 @@
 export const getGifs = async (category) => {
   try {
-    const apiKey = "9OxTEDQsrknlU3CNV3jtt44nNfiNFYEC";
+    const apiKey = "BsHg9wzHD3Al0i85X6llHO2zoqHfy78U";
     const url = `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${category}&limit=20`;
     const res = await fetch(url);
     const { data } = await res.json();
-    const gifs = data.map((img) => ({
-      id: img.id,
-      title: img.title,
-      url: img.url,
-    }));
+    const gifs = data.map(
+      ({
+        id,
+        title,
+        images: {
+          downsized_medium: { url },
+        },
+      }) => ({ id, title, url })
+    );
     return gifs;
   } catch (err) {
     console.log(err);
